2ebb720defdf110fa8359f3bc0213a3418392aec
almeidas-tatiane <almeidas.tatiane@gmail.com>
2025-05-26 10:52:56 -0300

    Adding Exercise 1

---
+++ jmeter-dsl/src/test/java/Exercise1.java
@@ -0,0 +1,20 @@
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+import java.io.IOException;
+import java.time.Duration;
+import org.junit.Test;
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
+
+public class Exercise1 {
+
+    @Test
+    public void testExercise1() throws IOException {
+        TestPlanStats stats = testPlan(
+                threadGroup(5,Duration.ofSeconds(10),
+                        httpSampler("https://httpbin.org/get"))
+
+        ).run();
+
+    }
+
+}
