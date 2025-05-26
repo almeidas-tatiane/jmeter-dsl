b29d3f925ec54348288b99e96677277242e855c7
almeidas-tatiane <almeidas.tatiane@gmail.com>
2025-05-26 15:40:55 -0300

    Adding Exercise 5 Training Assertion

--- jmeter-dsl/src/test/java/Exercise5.java
+++ jmeter-dsl/src/test/java/Exercise5.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Exercise5 {
+
+import org.junit.jupiter.api.Test;
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
+import java.io.IOException;
+
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
+// Training Assertions
+public class Exercise5 {
+    @Test
+    public void testExercise5() throws IOException {
+        TestPlanStats stats = testPlan(
+                threadGroup(1, 1,
+                        httpSampler("https://petstore.swagger.io/v2/pet/1")
+                                .children(
+                                        responseAssertion().containsSubstrings("doggie")
+                                )
+              ),
+                resultsTreeVisualizer()
+        ).run();
+
+
+
+
+
+
+    }
 }
