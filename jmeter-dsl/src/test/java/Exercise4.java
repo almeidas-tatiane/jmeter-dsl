1735ec9a56aa036e7a92c9198cb428c32e94469f
almeidas-tatiane <almeidas.tatiane@gmail.com>
2025-05-26 14:06:54 -0300

    Adding Exercise4

---
+++ jmeter-dsl/src/test/java/Exercise4.java
@@ -0,0 +1,32 @@
+import org.apache.http.entity.ContentType;
+import org.junit.Test;
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
+import java.io.IOException;
+
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
+//Training Login Flow and response extraction
+public class Exercise4 {
+
+    @Test
+    public void testExercise4() throws IOException {
+        TestPlanStats stats = testPlan(
+                threadGroup(1,1,
+                        httpSampler("https://httpbin.org/post")
+                                .post("{\"token\": \"123456\"}", ContentType.APPLICATION_JSON)
+                                .children(
+                                        jsonExtractor("TOKEN", "json.token")
+                                ),
+                        httpSampler("https://httpbin.org/bearer")
+                                .header("Authorization", "Bearer ${TOKEN}")
+
+                        ),
+                resultsTreeVisualizer()
+
+        ).run();
+    }
+
+
+
+}
